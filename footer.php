6f3f09df500e1a27da1753810acc216762c0e48a
florin <bucataruflorin89@gmail.com>
2019-01-22 16:21:29 +0000

    added date in footer

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         <?php 
         // Afișează ceva de felul: Monday 8th of August 2005 03:12:46 PM
         date_default_timezone_set('Europe/Bucharest');
-            echo date('l jS \of F Y h:i:s A');
+            echo date('Y');
         ?>
         </p>
       </div>
